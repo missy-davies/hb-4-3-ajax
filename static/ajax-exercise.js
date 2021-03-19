a00eaa44eb5041091f8a4a84e16a8c89619d69aa
Missy Davies <ms.melissadavies@gmail.com>
2021-03-19 05:05:31 +0000

    Add quantity melons function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -37,11 +37,25 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+function updateMelons(results) {
+    if (results.code === "OK") {
+        $('#order-status').html("<p>" + results.msg + "</p>");
+    } else {
+        $('#order-status').addClass("order-error");
+        $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+    }
+}
+
+
 function orderMelons(evt) {
     evt.preventDefault();
 
-    // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    let formInputs = {
+        "melon_type": $("#melon-type-field").val(),
+        "qty": $("#qty-field").val()
+    };
+    
+    $.post("/order-melons.json", formInputs, updateMelons);
 }
 
 $("#order-form").on('submit', orderMelons);
