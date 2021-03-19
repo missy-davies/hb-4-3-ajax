54f9b26a37a409c54e341e92ee783518c59afc3e
Missy Davies <ms.melissadavies@gmail.com>
2021-03-19 04:36:59 +0000

    Add show fortune function

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -5,7 +5,10 @@
 
 function showFortune(evt) {
 
-    // TODO: get the fortune and show it in the #fortune-text div
+    $.get('/fortune', (response) => {
+        $("#fortune-text").html(response);
+    });
+    
 }
 
 $('#get-fortune-button').on('click', showFortune);
